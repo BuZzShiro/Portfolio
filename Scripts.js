446257fc32e5fa8574b17c1bd3897597fc5beef3
BuZzShiro <BuZzShiro@gmail.com>
2023-05-02 15:56:55 +0100

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,5 +1,5 @@
 document.getElementById("TestResult").innerHTML = "Javascript first test";
 document.getElementById("TeskClick").onclick = function(NewModel) {ClickTest()};
 function ClickTest(NewModel) {
-  document.getElementById("TestResult").innerHTML = NewModel;
+  document.getElementById("MViewer").src = NewModel;
 }
