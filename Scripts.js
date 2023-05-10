398ff4397568e83304e0092a36e96a571df1c19c
BuZzShiro <BuZzShiro@gmail.com>
2023-05-10 11:35:07 +0100

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -3,3 +3,18 @@ document.getElementById("MImgLink").onclick = function(NewModel) {ChangeModel()}
 function ChangeModel(NewModel) {
   document.getElementById("MViewer").src = NewModel;
 }
+
+// Get the gallery box
+var imageBox1 = document.getElementById("imageBox1");
+
+// Get the modal image tag
+var modal = document.getElementById("myModal");
+
+var modalImage = document.getElementById("modal-image");
+
+// When the user clicks the big picture, set the image and open the modal
+imageBox1.onclick = function (e) {
+  var src = e.srcElement.src;
+  modal.style.display = "block";
+  modalImage.src = src;
+};
