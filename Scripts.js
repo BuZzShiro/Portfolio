8fa6b756291b656079f751092959de4d0592bfa5
BuZzShiro <BuZzShiro@gmail.com>
2023-05-14 16:16:21 +0100

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -33,6 +33,27 @@ imgArray[10].src = 'Images/Renders/Robot.png';
 imgArray[11] = new Image();
 imgArray[11].src = 'Images/Renders/Safe.png';
 
+function nextImage(element)
+{
+    var img = document.getElementById(element);
+
+    for(var i = 0;i<imgArray.length;i++)
+    {
+        if(imgArray[i] == img)
+        {
+            if(i == imgArray.length)
+            {
+                var j = 0;
+                document.getElementById(element).src = imgArray[j].src;
+                break;
+            }
+            else
+            var j = i + 1;
+            document.getElementById(element).src = imgArray[j].src;
+            break;
+        }
+    }   
+}
 //--------------------------------------------Show Render Images------------------------------------------------\\
 // Get the gallery box
 var imageBox1 = document.getElementById("imageBox1");
