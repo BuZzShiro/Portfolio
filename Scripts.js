348cfb146a85240a65353cc223b19ab09afabae4
BuZzShiro <BuZzShiro@gmail.com>
2023-05-14 15:43:48 +0100

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -1,9 +1,32 @@
+//---------------------------------------------Change Model-----------------------------------------------------\\
 document.getElementById("MImgLink").onclick = function(NewModel) {ChangeModel()};
 
 function ChangeModel(NewModel) {
   document.getElementById("MViewer").src = NewModel;
 }
 
+//---------------------------------------------Render Array-----------------------------------------------------\\
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = 'images/img/Splash_image1.jpg';
+
+imgArray[1] = new Image();
+imgArray[1].src = 'images/img/Splash_image2.jpg';
+
+imgArray[2] = new Image();
+imgArray[2].src = 'images/img/Splash_image3.jpg';
+
+imgArray[3] = new Image();
+imgArray[3].src = 'images/img/Splash_image4.jpg';
+
+imgArray[4] = new Image();
+imgArray[4].src = 'images/img/Splash_image5.jpg';
+
+imgArray[5] = new Image();
+imgArray[5].src = 'images/img/Splash_image6.jpg'
+
+//--------------------------------------------Show Render Images------------------------------------------------\\
 // Get the gallery box
 var imageBox1 = document.getElementById("imageBox1");
 
