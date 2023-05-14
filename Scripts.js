5db62cab39fa131c10b988b0b62076f7c029ddc6
BuZzShiro <BuZzShiro@gmail.com>
2023-05-14 16:15:27 +0100

    Update Scripts.js

--- Scripts.js
+++ Scripts.js
@@ -9,22 +9,29 @@ function ChangeModel(NewModel) {
 var imgArray = new Array();
 
 imgArray[0] = new Image();
-imgArray[0].src = 'images/img/Splash_image1.jpg';
-
+imgArray[0].src = 'Images/Renders/ZeroBio.png';
 imgArray[1] = new Image();
-imgArray[1].src = 'images/img/Splash_image2.jpg';
-
+imgArray[1].src = 'Images/Renders/Museum.png';
 imgArray[2] = new Image();
-imgArray[2].src = 'images/img/Splash_image3.jpg';
-
+imgArray[2].src = 'Images/Renders/Room.png';
 imgArray[3] = new Image();
-imgArray[3].src = 'images/img/Splash_image4.jpg';
-
+imgArray[3].src = 'Images/Renders/Diamond.png';
 imgArray[4] = new Image();
-imgArray[4].src = 'images/img/Splash_image5.jpg';
-
+imgArray[4].src = 'Images/Renders/Planet.png';
 imgArray[5] = new Image();
-imgArray[5].src = 'images/img/Splash_image6.jpg'
+imgArray[5].src = 'Images/Renders/PotMirror.png';
+imgArray[6] = new Image();
+imgArray[6].src = 'Images/Renders/SecurityCam.png';
+imgArray[7] = new Image();
+imgArray[7].src = 'Images/Renders/FancyCieling.png';
+imgArray[8] = new Image();
+imgArray[8].src = 'Images/Renders/MedicalLab.png';
+imgArray[9] = new Image();
+imgArray[9].src = 'Images/Renders/NekoRoom.png';
+imgArray[10] = new Image();
+imgArray[10].src = 'Images/Renders/Robot.png';
+imgArray[11] = new Image();
+imgArray[11].src = 'Images/Renders/Safe.png';
 
 //--------------------------------------------Show Render Images------------------------------------------------\\
 // Get the gallery box
